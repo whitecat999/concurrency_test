0915b6595f50d9f037385e64a0e7e66faf098571
whitecat <wzy999157@163.com>
2019-05-04 23:02:09 +0800

    用于辨识的注解类

---
+++ src/main/java/com/wzy/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,19 @@
+package com.wzy.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @program: concurrency
+ * @description: ${description}
+ * @author: WangZiYu
+ * @create: 2019-05-04 22:40
+ **/
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    String value() default "";
+}
